7d50a0b2b08572bf220035aaee7d057c5d9b0e0a
MaiFahad <71163477+MaiFahad@users.noreply.github.com>
2021-09-02 01:13:14 +0300

    Create BaseResult.php

---
+++ BaseResult.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+
+
+try{
+  $conn = new PDO("mysql:host=$servername;dbname=robotbase", $username);
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->beginTransaction();
+	
+$val=$conn->prepare("SELECT RLFBS FROM `directions` ORDER BY id DESC LIMIT 1");		
+$val->execute();	
+$r=$val->fetchAll();
+
+foreach($r as $m) {
+echo "LAST ENTERY VALUE: ".$m['RLFBS'];
+}
+} catch(PDOException $e) {
+
+  $conn->rollback();
+  echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
+
+
+?>
